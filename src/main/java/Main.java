0ec9f996fbeaaa94fe0bc5146654009c6d088ba0
uberVelocity <mihaitu.popescu@gmail.com>
2018-08-28 08:39:04 +0300

    New Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,6 @@ public class Main {
         System.out.println(model.getPlayer().getPlayerUnits());
         /**/
         frame.add(resourcePanel);
-        frame.setVisible(false);
+        frame.setVisible(true);
     }
 }
